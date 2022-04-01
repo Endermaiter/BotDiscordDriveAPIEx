2f020bf17d7fbdc308a655a51d2cd7b6a053a0db
Endermaiter <mpereznovoa@danielcastelao.org>
2022-04-01 09:33:17 +0200

    Embed funcional

--- src/main/java/BotMain.java
+++ src/main/java/BotMain.java
@@ -1,8 +1,15 @@
+import discord4j.common.util.Snowflake;
 import discord4j.core.DiscordClient;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.channel.GuildMessageChannel;
 import discord4j.core.object.entity.channel.MessageChannel;
+import discord4j.core.spec.EmbedCreateSpec;
+import discord4j.core.spec.MessageCreateSpec;
+import discord4j.rest.util.Color;
+
+import java.time.Instant;
 
 public class BotMain {
 
@@ -11,16 +18,42 @@ public class BotMain {
         final DiscordClient client = DiscordClient.create(token);
         final GatewayDiscordClient gateway = client.login().block();
 
+        //ping pong
+
         gateway.on(MessageCreateEvent.class).subscribe(event -> {
             final Message message = event.getMessage();
-            if ("!ping".equals(message.getContent())) {
+            if ("/ping".equals(message.getContent())) {
                 final MessageChannel channel = message.getChannel().block();
-                channel.createMessage("Pong!").block();
+                channel.createMessage("Pong").block();
+            }
+        });
+
+        //EMBED
+
+        EmbedCreateSpec embed = EmbedCreateSpec.builder()
+                .color(Color.BLUE)
+                .title("Title")
+                .url("https://discord4j.com")
+                .author("EL MARCOS TODO UN CAPO", "https://discord4j.com", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDFFi8xZQpVW3GJRv2geK9u54vbbGlXYn4IA&usqp=CAU")
+                .description("FORTINAITI LABABAYÈ")
+                .thumbnail("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDFFi8xZQpVW3GJRv2geK9u54vbbGlXYn4IA&usqp=CAU")
+                .addField("FUCHEBOL", "fuchebol", false)
+                .addField("\u200B", "\u200B", false)
+                .addField("MESSI", "messi", true)
+                .image("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDFFi8xZQpVW3GJRv2geK9u54vbbGlXYn4IA&usqp=CAU")
+                .timestamp(Instant.now())
+                .footer("footer", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDFFi8xZQpVW3GJRv2geK9u54vbbGlXYn4IA&usqp=CAU")
+                .build();
+
+        gateway.on(MessageCreateEvent.class).subscribe(event -> {
+            final Message message = event.getMessage();
+            if ("/embed".equals(message.getContent())) {
+                final MessageChannel channel = message.getChannel().block();
+                channel.createMessage(embed).block();
             }
         });
 
         gateway.onDisconnect().block();
-
     }
 }
 
